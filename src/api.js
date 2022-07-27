e86d94bdce7ed240e048003f9683e8429ee4fe45
Matej <matej.galic.ets@gmail.com>
2022-07-27 15:49:39 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ce3c01115cmshfc0a3ba1ded7f50p116a88jsnd30ae401b38a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "ba8f445d0de4ebd39b08d5cce74d456c"
